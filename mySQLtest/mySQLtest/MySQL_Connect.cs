262bdf06e5b98a492b65af62ff564ecec065e139
macpod1994 <maciekp10@interia.pl>
2017-12-12 23:29:03 +0100

    C#

---
+++ mySQLtest/mySQLtest/MySQL_Connect.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace mySQLtest
+{
+    class MySQL_Connect
+    {
+
+            private MySqlConnection connection;
+            private string server;
+            private string database;
+            private string uid;
+            private string password;
+
+            //Constructor
+            public MySQL_Connect()
+            {
+                Initialize();
+            }
+
+            //Initialize values
+            private void Initialize()
+            {
+                server = "localhost";
+                database = "scada";
+                uid = "root";
+                password = "qwerty";
+                string connectionString;
+                connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+                connection = new MySqlConnection(connectionString);
+            }
+
+        //open connection to database
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string query)
+        {
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        ////Update statement
+        //public void Update()
+        //{
+        //}
+
+        ////Delete statement
+        //public void Delete()
+        //{
+        //}
+
+        //Select statement
+        public List<string>[] SelectLimitsList(int ssId)
+        {
+            string query = "SELECT * FROM scada.limits tab WHERE tab.state_space_id="+ssId.ToString();
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["tag"] + "");
+                    list[1].Add(dataReader["state_space_id"] + "");
+                    list[2].Add(dataReader["value"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Limit statement
+        public float LimitValue(int ssId, String limitTag)
+        {
+            string query = "SELECT value FROM scada.limits tab WHERE tab.tag=" + limitTag;
+            float value = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                value = float.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return value;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        ////Backup
+        //public void Backup()
+        //{
+        //}
+
+        ////Restore
+        //public void Restore()
+        //{
+        //}
+
+
+    }
+}
